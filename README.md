720b7ead4e139adadd73137dbf6bb651f8976198
Artem Onyshchenko <temafey@gmail.com>
2018-12-03 14:23:01 +0200

    Redis Cluster

---
+++ README.md
@@ -0,0 +1,26 @@
+Redis Cluster Tutorial
+======================
+
+This is the [official Redis tutorial](https://redis.io/topics/cluster-tutorial) I follow to build the cluster basically.
+
+The redis containers are based on alpine of course and are optimized to use the minimum of possible layers to be configurable.
+
+    docker-compose build
+    docker-compose up -d
+ 
+    docker run -it temafey/redis-trib sh -c "echo 'yes' | ./redis-trib.rb create --replicas 1 192.168.99.100:7001 192.168.99.100:7002 192.168.99.100:7003 192.168.99.100:6000 192.168.99.100:6001 192.168.99.100:6002"
+    
+Done.
+
+Testing the cluster:
+       
+    redis-cli -p 7001 cluster nodes
+    docker kill rediscluster_master2_1
+    redis-cli -p 7001 cluster nodes
+    
+# K8s
+
+Require **envsubst** and **kubectl**
+
+    ./k8s/boot.sh --help
+    ./k8s/boot.sh -i=NUM_INSTANCES -img=DOCKER_IMAGE --replicas=3
---
+++ k8s/boot.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+INSTANCES=6
+IMAGE=temafey/redis-cluster-instance:latest
+REPLICAS=1
+CLEAN=true
+SERVICES=''
+
+for i in "$@"
+do
+case $i in
+    -i=*|--instances=*)
+        INSTANCES="${i#*=}"
+    ;;
+    -img=*|--image=*)
+        IMAGE="${i#*=}"
+    ;;
+    -r=*|--replicas=*)
+        REPLICAS="${i#*=}"
+    ;;
+    ---no-clean)
+        CLEAN=false
+    ;;
+    --help)
+        echo "Kubernetes Redis cluster bootstrapping."
+        echo ""
+        echo "-i|--instances    Number of instances. Default -i=6"
+        echo "-r|--replicas     Number of cluster replicas. Default -r=1"
+        echo "-img|--image      Docker image. Default -img=temafey/redis-cluster-instance:latest"
+        echo "--no-clean        Avoid remove old cluster."
+        echo ""
+        exit;
+    ;;
+    *)
+    ;;
+esac
+done
+
+if $CLEAN; then
+    echo "Cleaning old cluster"
+
+    if kubectl get deploy/redis-0 &> /dev/null; then
+
+        kubectl delete po/redis-c
+        kubectl delete -f k8s/deployment
+        kubectl delete -f k8s/service
+    fi
+
+    rm -rf k8s/deployment/deployment-*
+    rm -rf k8s/service/service-*
+fi
+
+for ((i=0;i<=INSTANCES-1;i++)); do
+    export INSTANCE_NUMBER=$i;
+    export IMAGE=$IMAGE;
+
+    echo "Creating node service: $i";
+    envsubst <k8s/deployment/deployment> k8s/deployment/deployment-${INSTANCE_NUMBER}.yml;
+    envsubst <k8s/service/service> k8s/service/service-${INSTANCE_NUMBER}.yml;
+
+
+done
+
+kubectl create -f k8s/deployment
+kubectl create -f k8s/service
+
+echo "Waiting for ip assignations"
+sleep 5
+
+for i in `kubectl get svc | grep redis | awk '{print $2}'`; do
+
+    SERVICES=$SERVICES" $i:6379"
+done
+
+echo "Bootstrapping the cluster"
+kubectl run redis-c --image=temafey/redis-trib --restart=Never --command -- sh -c "echo 'yes' | ./redis-trib.rb create --replicas $REPLICAS $SERVICES"
